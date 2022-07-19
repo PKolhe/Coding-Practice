007e7ae08d0c8da22be4f99c9b47fcc07262abc2
Pankaj Kolhe <kolhe.pankaj@live.com>
2022-07-19 11:43:49 +0530

    Changes Done.

---
+++ 1.Array_Find Maximum in Sliding Window.js
@@ -0,0 +1,14 @@
+/*
+Problem Statement:
+Given an integer array and a window of size w, find the current maximum value 
+in the window as it slides through the entire array.
+Note: If the window size is greater than the array size, we will consider 
+the entire array as a single window.
+
+Sample input:
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+window_size = 3
+
+Expected output:
+[3, 4, 5, 6, 7, 8, 9, 10] 
+*/
